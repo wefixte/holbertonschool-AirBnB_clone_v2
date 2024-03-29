773088ddf439c8f7533405a4cf71411a4b09f2a4
MLK59 <abdelmalekmhamed@gmail.com>
2024-03-29 13:58:18 +0100

    Update init.py:  Add conditional for HBNB_TYPE_STORAGE

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+"""This module instantiates an object of class FileStorage
+initialize the models package"""
+
+import os
+from models.db_storage import DBStorage  # Import de la classe DBStorage
+from models.file_storage import FileStorage  # Import de la classe FileStorage
 
 
-storage = FileStorage()
-storage.reload()
+if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
+    storage = DBStorage()  # Création d'une instance DBStorage
+    storage.reload()  # Rechargement des modèles depuis la base de données
+else:
+    storage = FileStorage()  # Création d'une instance FileStorage
+    storage.reload()  # Rechargement des modèles depuis les fichiers
