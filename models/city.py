90eb599287bbf53b9d1ae876b4b57a4555efbd10
MLK59 <abdelmalekmhamed@gmail.com>
2024-03-29 13:54:50 +0100

    Update City:  Add tablename, name, state_id.

--- models/city.py
+++ models/city.py
@@ -5,5 +5,16 @@ from models.base_model import BaseModel
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    if models.storage_t == "db":
+        __tablename__ = 'cities'
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        places = relationship("Place", backref="cities", cascade="all, delete-orphan")
+
+    else:
+        state_id = ""
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes city"""
+        super().__init__(*args, **kwargs)
