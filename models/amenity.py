32d14a4ef71b414f5dd181c13aa04118c25afc62
MLK59 <abdelmalekmhamed@gmail.com>
2024-03-29 14:24:12 +0100

    Update : Added tablename and attribute: name. Added place_amenities representing a Many-To-Many relationship with Place.

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,29 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+""" State Module for HBNB project
+holds class Amenity
+"""
+
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """
+    subclass - Amenity : inherits from BaseModel
+    """
+    if models.storage_t == 'db':
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize public class attribute:
+        name - string - empty string
+        """
+        super().__init__(*args, **kwargs)
