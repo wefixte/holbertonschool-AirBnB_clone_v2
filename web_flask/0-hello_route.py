01fd16e7814b5fa0f153747632ef70e51d293ea3
wefixte <cassy.pernelle@gmail.com>
2024-04-10 17:38:36 +0200

    ?

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-""" script that starts a Flask web application """
+""" Script that starts a Flask web application """
 
 
 from flask import Flask
+
 # Create an instance with name of the current module
 app = Flask(__name__)
 
