9ccd9d4620c6543179f203f08eb49ae58e153030
wefixte <cassy.pernelle@gmail.com>
2024-04-10 18:08:59 +0200

    fixed style blankline

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -31,5 +31,6 @@ def pythontxt(text):
     """ Display string string when access /python route"""
     return "Python {}".format(text.replace("_", " "))
 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
