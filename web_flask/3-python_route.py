47e3744a0b64548649af328435bfbfab1053ebb9
wefixte <cassy.pernelle@gmail.com>
2024-04-10 18:08:08 +0200

    add t3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Script that starts a Flask web application """
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """ Display "Hello HBTN!" when access """
+    return "Hello HBTN!"
+
+
+@app.route('/hbtn', strict_slashes=False)
+def hbtn():
+    """ Display "HBTN" when access /hbtn route """
+    return "HBTN"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cfun(text):
+    """ Display string when access /c/<text> route """
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False)
+def pythontxt(text):
+    """ Display string string when access /python route"""
+    return "Python {}".format(text.replace("_", " "))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
