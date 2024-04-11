694989ade6de091d8c8157a530bb8f13729146c4
wefixte <cassy.pernelle@gmail.com>
2024-04-11 10:56:26 +0200

    test

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python3
-""" Script that starts a Flask web application """
-
-
+""" Start a Flask web application
+"""
 from flask import Flask
 
-# Create an instance with name of the current module
+
 app = Flask(__name__)
 
 
-# Flask decorator, bind root URL '/' to hello() function
 @app.route('/', strict_slashes=False)
-def hello():
-    """ Display "Hello HBTN!" when access """
-    return "Hello HBTN!"
+def hello_hbnb():
+    """ Display Hello HBNB!
+    """
+    return "Hello HBNB!"
 
 
-# Check if script is executed as main program
 if __name__ == "__main__":
-    # Start Flask dev server.
-    # Listens all network interfaces (0.0.0.0) on port 5000
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
