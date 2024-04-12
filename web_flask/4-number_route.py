dd22c98252a1508f914281a1896abfceb64c4f6e
wefixte <cassy.pernelle@gmail.com>
2024-04-12 17:40:00 +0200

    change hbtn to hbnb

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -8,15 +8,17 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
-    """ Display "Hello HBTN!" when access """
-    return "Hello HBTN!"
+def hello_hbnb():
+    """ Display Hello HBNB!
+    """
+    return "Hello HBNB!"
 
 
-@app.route('/hbtn', strict_slashes=False)
-def hbtn():
-    """ Display "HBTN" when access /hbtn route """
-    return "HBTN"
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Display HBNB
+    """
+    return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
