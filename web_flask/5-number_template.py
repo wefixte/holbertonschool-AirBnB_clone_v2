ee12dd3260fda1f0433163028a94676c1eb3537f
wefixte <cassy.pernelle@gmail.com>
2024-04-12 17:49:33 +0200

    Add route for template HTML and HTML template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+""" Script that starts a Flask web application """
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Display Hello HBNB!
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Display HBNB
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cfun(text):
+    """ Display string when access /c/<text> route """
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python/', defaults={"text": "is cool"}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythontxt(text):
+    """ Display string string when access /python route """
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route('/number/<int:value>', strict_slashes=False)
+def number(value):
+    """ Display integer when access /number route """
+    return "{} is a number".format(value)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """ Display HTML page only if n is an integer """
+    return render_template("5-number.html", n=n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
